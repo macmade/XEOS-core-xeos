122598ababdd9226f3c417df5fa4838219387724
Macmade <macmade@eosgarden.com>
2012-11-19 22:27:55 +0100

    FS and GS are now also set.

--- XEOS_Stub.32.s
+++ XEOS_Stub.32.s
@@ -111,6 +111,8 @@ start:
     mov     ax,         0x10
     mov     ds,         ax
     mov     es,         ax
+    mov     fs,         ax
+    mov     gs,         ax
     
     ; Sets up the stack space
     mov     ss,         ax
